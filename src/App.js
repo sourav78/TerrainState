fe64f0dab00a4cccb9215faaf484f6dead032086
sourav78 <souravranjan488@gmail.com>
2023-11-09 20:27:54 +0530

    add some assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
 function App() {
+
+  const key1 = process.env.REACT_APP_WEATHER_KEY1
+  const key2 = process.env.REACT_APP_WEATHER_KEY2
+  const key3 = process.env.REACT_APP_WEATHER_KEY3
+  const key4 = process.env.REACT_APP_WEATHER_KEY4
+
     return (
         <>
           I am julu
